e0f10161462641a523eb8026d52b02db4bb4c1d0
Paraschivoi <paraschivoi.bogdan@gmail.com>
2023-11-01 09:42:39 +0100

    sasiu string nu merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -1,12 +1,10 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
 import org.firstinspires.ftc.teamcode.RobotHardware;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +13,14 @@ import java.util.List;
 public class TeleOP extends OpMode {
 
     RobotHardware robot = new RobotHardware(this);
-    SampleMecanumDrive drive;
+
 
 @Override
 public void init(){
     robot.init();
-    drive = new SampleMecanumDrive(hardwareMap);
+
 }
 public void loop() {
-    drive.update();
     robot.movement(gamepad1);
 }
 }
