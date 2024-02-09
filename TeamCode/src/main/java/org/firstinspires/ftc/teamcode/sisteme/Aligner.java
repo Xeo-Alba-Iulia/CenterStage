1a6e8699764d5ea2dd6bb85dce7392cebe41a7eb
Paraschivoi <paraschivoi.bogdan@gmail.com>
2024-02-09 11:14:25 +0200

    regio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sisteme/Aligner.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.sisteme;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Aligner {
+    Servo al1, al2;
+    public Aligner(Servo al1, Servo al2) {
+        this.al1 = al1;
+        this.al2 = al2;
+    }
+
+    public void setPosition(double position) {
+        al1.setPosition(position);
+        al2.setPosition(position);
+    }
+
+    public double getPosition() {
+
+        return al1.getPosition();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sisteme/Ridicare.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.sisteme;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.utilities.PIDController;
+
+public class Ridicare {
+    DcMotor ridicare1;
+    DcMotor ridicare2;
+    public PIDController controller;
+
+    //pozitii ridicare
+    public static final int POS_1 = 2300;
+    public static final int POS_2 = 4600;
+    public static final int POS_3 = 6900;
+    public int target;
+
+    public Ridicare (DcMotor ridicare1, DcMotor ridicare2) {
+        this.ridicare1 = ridicare1;
+        this.ridicare2 = ridicare2;
+        target = 0;
+        controller = new PIDController(0.024, 0.007955396742, 0);
+    }
+
+    /**
+     * Give power to both lift motors
+     * @param power power given to both motors
+     */
+    public void setPower(double power) {
+        ridicare1.setPower(power);
+        ridicare2.setPower(power);
+    }
+
+    public double getPower() {
+        return (ridicare1.getPower() + ridicare2.getPower()) / 2.0;
+    }
+
+    /**
+     * Get position of the motor with the encoder attached
+     * @return ridicare1's current encoder's ticks
+     */
+    public int getCurrentPosition() {
+        return ridicare1.getCurrentPosition();
+    }
+
+
+
+    /**
+     * Reset both encoder's ticks
+     * even if one of them is redundant, for safety
+     */
+    public void resetEncoder() {
+        ridicare1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ridicare2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    /**
+     * Update the power given by the pid controller
+     */
+    public void update() {
+        double power = controller.update(target, ridicare1.getCurrentPosition());
+        ridicare1.setPower(power);
+        ridicare2.setPower(power);
+    }
+}
